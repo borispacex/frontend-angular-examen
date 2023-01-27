db207b6e19ecb599ad65ab9552a0a00cb1d06817
Boris Vargas <borisvargaspaucara@gmail.com>
2023-01-27 04:11:41 -0400

    endPoint URL Api correguido

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,2 +1,2 @@
 // export const endPoint: string  = 'http://localhost:8081/api/v1/';
-export const endPoint: string  = 'https://ms-examen.azurewebsites.net//api/v1/';
+export const endPoint: string  = 'https://ms-examen.azurewebsites.net/api/v1/';
